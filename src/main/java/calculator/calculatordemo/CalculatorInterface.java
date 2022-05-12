0cd475809debb0b4f2b1e197c6467dc738f0d288
Sam Smith <romanmissilin@mail.ru>
2022-05-12 17:00:47 +0300

    Готово

---
+++ src/main/java/calculator/calculatordemo/CalculatorInterface.java
@@ -0,0 +1,14 @@
+package calculator.calculatordemo;
+
+public interface CalculatorInterface {
+
+    String Welcome();
+
+    String Addition(int num1, int num2);
+
+    String Subtraction(int num1, int num2);
+
+    String Multiplication(int num1, int num2);
+
+    String Division(int num1, int num2);
+}
---
+++ src/main/java/calculator/calculatordemo/CalculatorService.java
@@ -0,0 +1,35 @@
+package calculator.calculatordemo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService implements CalculatorInterface {
+
+    public String Welcome() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public String Addition(int num1, int num2) {
+        int sum = 0;
+        sum = num1 + num2;
+        return num1 + "+" + num2 + "=" +  sum;
+    }
+
+    public String Subtraction(int num1, int num2) {
+        int sum = 0;
+        sum = num1 - num2;
+        return num1 + "-" + num2 + "=" + sum;
+    }
+
+    public String Multiplication(int num1, int num2) {
+        int sum = 0;
+        sum = num1 * num2;
+        return num1 + "*" + num2 + "=" + sum;
+    }
+
+    public String Division(int num1, int num2) {
+        int sum = 0;
+        sum = num1 / num2;
+        return num1 + "/" + num2 + "=" + sum;
+    }
+}
